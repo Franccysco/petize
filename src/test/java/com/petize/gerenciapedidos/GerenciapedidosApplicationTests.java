6ffdef984c422ab57af66f761f5141e951505d12
Francisco Sousa <fchagas.sousa45@gmail.com>
2024-08-24 23:49:16 -0300

    initial commit

---
+++ src/test/java/com/petize/gerenciapedidos/GerenciapedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petize.gerenciapedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciapedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
